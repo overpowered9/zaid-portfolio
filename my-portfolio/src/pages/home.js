2d1f1fbd15f4cb217f3d47d1ae86a3c9391db06f
overpowered9 <117577891+overpowered9@users.noreply.github.com>
2024-06-12 12:41:01 +0500

    commit

--- my-portfolio/src/pages/home.js
+++ my-portfolio/src/pages/home.js
@@ -1,40 +1,43 @@
-import Header from '../components/header';
-import Intro from '../components/intro/intro';
-import AnimatedImage from '../components/animated_/animatedimage';
-import AnimatedAttr from '../components/animated_/animated_attributes';
-import AnimatedAttrSC from '../components/animated_/animated_Scroll';
-import ContactUS from '../components/Contactus';
-import { useState } from 'react';
-import { useEffect } from 'react';
+import Header from "../components/header";
+import Intro from "../components/intro/intro";
+import AnimatedImage from "../components/animated_/animatedimage";
+import AnimatedAttr from "../components/animated_/animated_attributes";
+import AnimatedAttrSC from "../components/animated_/animated_Scroll";
+import ContactUS from "../components/Contactus";
+import { useState } from "react";
+import { useEffect } from "react";
+import PortFolio from "../components/portfolio";
 function HomePage() {
   const [isScreenSmall, setIsScreenSmall] = useState(window.innerWidth < 1000);
   useEffect(() => {
     const handleResize = () => {
       setIsScreenSmall(window.innerWidth < 1000);
     };
-    window.addEventListener('resize', handleResize);
+    window.addEventListener("resize", handleResize);
     return () => {
-      window.removeEventListener('resize', handleResize);
+      window.removeEventListener("resize", handleResize);
     };
   }, []);
-  console.log("server is on")
+  console.log("server is on");
   return (
     <>
-    <div className=" bg-[#245167]">
-    <Header></Header>
-    <div className="ml-0 h-[88vh] flex flex-col justify-between sm:flex-row sm:h-auto overflow-hidden md:ml-[180px]">
-  <Intro></Intro>
-  <AnimatedImage></AnimatedImage>
-  
-</div>
-    </div>
-    <div className=' w-[100%] overflow-hidden'>
-        {isScreenSmall?<AnimatedAttrSC/>:<AnimatedAttr/>}
-   
-   </div>
-   <div><ContactUS></ContactUS></div>
+      <div className=" bg-[#245167]">
+        <Header></Header>
+        <div className="ml-0 h-[88vh] flex flex-col justify-between sm:flex-row sm:h-auto overflow-hidden md:ml-[180px]">
+          <Intro></Intro>
+          <AnimatedImage></AnimatedImage>
+        </div>
+      </div>
+      <div className=" w-[100%] overflow-hidden">
+        {isScreenSmall ? <AnimatedAttrSC /> : <AnimatedAttr />}
+      </div>
+      <div>
+        <PortFolio></PortFolio>
+      </div>
+      <div>
+        <ContactUS></ContactUS>
+      </div>
     </>
-    
   );
 }
 
