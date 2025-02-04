21ae3f2879f2d8df1e440556fc9e24cc632093c1
overpowered9 <117577891+overpowered9@users.noreply.github.com>
2025-02-04 22:30:03 +0500

    Update home.js

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,14 +1,17 @@
 import Header from "../components/header";
 import Intro from "../components/intro/intro";
+import StatView from "../components/stats";
 import AnimatedAttr from "../components/animated_/animated_attributes";
 import AnimatedAttrSC from "../components/animated_/animated_Scroll";
 import ContactUS from "../components/Contactus";
 import { useState } from "react";
 import { useEffect } from "react";
-import PortFolio from "../components/portfolio";
+
 import Technologies2 from "../components/Technologies copy";
 import AnimatedImage2 from "../components/animated_/animatedimage2";
 import PortfolioCarousel from "../components/portfolio copy 2";
+import AnimatedTechnologies from "../components/Technologies";
+import InteractiveCV from "../components/CV";
 function HomePage() {
   const [isScreenSmall, setIsScreenSmall] = useState(window.innerWidth < 1000);
   useEffect(() => {
@@ -38,9 +41,12 @@ function HomePage() {
       <div id="Technologies">
         <Technologies2></Technologies2>
       </div>
-      <div className=" w-[100%] overflow-hidden">
-        {isScreenSmall ? <AnimatedAttrSC /> : <AnimatedAttr />}
+      <div className="flex flex-col align-middle justify-center w-[100%] pl-[10%] pr-[10%] sm:flex-row ">
+        {/* {isScreenSmall ? <AnimatedAttrSC /> : <AnimatedAttr />} */}
+        <StatView />
       </div>
+      {/* <InteractiveCV /> */}
+
       <div id="contact" className="pt-7">
         <ContactUS></ContactUS>
       </div>
